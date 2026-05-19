55e36b684558f1f34c95611c80ebc3e4054cfc37
abavoil <17646791+abavoil@users.noreply.github.com>
2026-05-19 08:47:14 +0000

    feat: automate repository setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using Test
 using Aqua
-using CTAppTemplate
+using CT-BK-App
 
 #
-@testset verbose = true showtiming = true "CTAppTemplate tests" begin
+@testset verbose = true showtiming = true "CT-BK-App tests" begin
     for name in (:aqua, :default)
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
--- test/test_default.jl
+++ test/test_default.jl
@@ -1,4 +1,4 @@
 # test_default
 function test_default()
-    @test CTAppTemplate.greet() isa Nothing
+    @test CT-BK-App.greet() isa Nothing
 end
